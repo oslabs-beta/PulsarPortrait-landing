d5d323aec3a55dcd00cba937cd977b4f1b76daee
cyduckk <cyrux@live.com>
2023-07-18 19:44:43 -0700

    adds reaction for when the copy clipboard is clicked

--- index.tsx
+++ index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './client/styles/styles.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App />
   </>
-) 
+);
